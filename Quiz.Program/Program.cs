8ad7b0b80da4c2264a91c9582112f0a9d22196c7
karl-soderberg <118443902+karl-soderberg@users.noreply.github.com>
2024-02-26 12:52:58 +0100

    restructure Dictionary

--- Quiz.Program/Program.cs
+++ Quiz.Program/Program.cs
@@ -1,14 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-Dictionary<string, string> questions = new Dictionary<string, string>()
+Dictionary<string, Dictionary<string, bool>> questions = new Dictionary<string, Dictionary<string, bool>>()
 
 {
-    { "What is the capital of France?", "Paris"},
-    { "What is the capital of Sweden", "Stockholm"}
+    {   
+        "What is the capital of France?", 
+        new Dictionary<string, bool>()
+        {
+            {"A: Marseille", false},
+            {"B: London", false},
+            {"C: Stockholm", false},
+            {"C: Paris", true},
+        }
+    },
+    {   "What is the capital of France?", 
+        new Dictionary<string, bool>()
+        {
+            {"A: 9", false},
+            {"B: 8", true},
+            {"C: 10", false},
+            {"C: 7", false},
+        }
+    },
 };
+
 
 foreach (var question in questions)
 {
     Console.WriteLine(question.Key);
+    Console.WriteLine("Select your option");
 }

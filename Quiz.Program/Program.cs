d7c60e66a91c8a32fd1e7525b7bf30a47eca58e4
karl-soderberg <118443902+karl-soderberg@users.noreply.github.com>
2024-02-26 14:05:28 +0100

    New approach hard code it all

--- Quiz.Program/Program.cs
+++ Quiz.Program/Program.cs
@@ -1,33 +1,75 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Microsoft.VisualBasic;
 
-Dictionary<string, Dictionary<string, bool>> questions = new Dictionary<string, Dictionary<string, bool>>()
 
+
+// Dictionary<string, Dictionary<string, bool>> questions = new Dictionary<string, Dictionary<string, bool>>()
+// {
+//     {   
+//         "What is the capital of France?", 
+//         new Dictionary<string, bool>()
+//         {
+//             {"A: Marseille", false},
+//             {"B: London", false},
+//             {"C: Stockholm", false},
+//             {"C: Paris", true},
+//         }
+//     },
+//     {   "How many planets are there in the solarsystem", 
+//         new Dictionary<string, bool>()
+//         {
+//             {"A: 9", false},
+//             {"B: 8", true},
+//             {"C: 10", false},
+//             {"C: 7", false},
+//         }
+//     },
+// };
+
+int i = 1;
+while(i==1)
 {
-    {   
-        "What is the capital of France?", 
-        new Dictionary<string, bool>()
+    int score = 0;
+    Console.WriteLine("How many planets are there in the solarsystem?");
+    Console.WriteLine("A:7 \nB:9 \nC:8 \nD:10\n");
+    Console.WriteLine("Select option 'A', 'B', 'C' or 'D'");
+    var input = Console.ReadLine();
+    if (input == "c" || input == "C")
+    {
+        Console.WriteLine("Correct! The answer is C:8");
+        score ++;
+    }
+    if (input == "a" || input == "A" ||
+        input == "b" || input == "B" || 
+        input == "d" || input == "D")
+    {
+        Console.WriteLine("Wrong! The answer is C:8");
+    }
+    else
         {
-            {"A: Marseille", false},
-            {"B: London", false},
-            {"C: Stockholm", false},
-            {"C: Paris", true},
+            Console.WriteLine("Invalid input");
         }
-    },
-    {   "What is the capital of France?", 
-        new Dictionary<string, bool>()
+
+    Console.WriteLine("Which planet is closes to the sun?");
+    Console.WriteLine("A:Venus \nB:Earth \nC:Mercury \nD:Mars\n");
+    Console.WriteLine("Select option 'A', 'B', 'C' or 'D'");
+    var input2 = Console.ReadLine();
+    if (input2 == "a" || input2 == "A")
+    {
+        Console.WriteLine("Correct! The answer is A:Venus");
+        score ++;
+    }
+    if (input2 == "b" || input2 == "B" ||
+        input2 == "c" || input2 == "C" ||
+        input2 == "d" || input2 == "D")
+    {
+        Console.WriteLine("Wrong! The answer is C:8");
+    }
+    else
         {
-            {"A: 9", false},
-            {"B: 8", true},
-            {"C: 10", false},
-            {"C: 7", false},
+            Console.WriteLine("Invalid input");
         }
-    },
-};
 
-
-foreach (var question in questions)
-{
-    Console.WriteLine(question.Key);
-    Console.WriteLine("Select your option");
+    
+    i++;
 }

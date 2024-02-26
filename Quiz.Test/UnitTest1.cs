bf4dfbb438ae660772fb44a91aa159bc69fe093b
karl-soderberg <118443902+karl-soderberg@users.noreply.github.com>
2024-02-26 14:37:28 +0100

    break out second question as a function

--- Quiz.Test/UnitTest1.cs
+++ Quiz.Test/UnitTest1.cs
@@ -1,3 +1,5 @@
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
 namespace Quiz.Test;
 
 public class UnitTest1
@@ -5,6 +7,10 @@ public class UnitTest1
     [Fact]
     public void Question1_should_return1_if_correct()
     {
+        string rightanswer = "A";
+
+        int result = QuizPlayclass.Question1();
+        
 
 
     }
